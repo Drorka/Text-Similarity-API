67a0b441334b99d56b4d76d5c400b7e7d1e8a123
Drorka~ <drorpa4@gmail.com>
2023-04-13 15:07:26 +0300

    reorganize directory structure

---
+++ src/app/layout.tsx
@@ -0,0 +1,28 @@
+import '@/styles/globals.css'
+import { Inter } from 'next/font/google'
+import { cn } from '@/src/lib/utils'
+import Providers from '../components/Providers'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function RootLayout({
+	children,
+}: {
+	children: React.ReactNode
+}) {
+	return (
+		<html
+			lang="en"
+			className={cn('bg-white text-slate-900 antialiased', inter.className)}
+		>
+			<body className="mi-h-screen bg-slate-50 dark:bg-slate-900 antialiased">
+				<Providers>
+					{children}
+					<NavBar />
+				</Providers>
+
+				<div className="h-40 md:hidden" />
+			</body>
+		</html>
+	)
+}
